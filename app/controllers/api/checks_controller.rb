4272e24cca0bcc2c354bbacd7655ba319a085588
Sergey <ser_8904@mail.ru>
2022-07-26 20:34:34 +0300

    fix webhooks

--- app/controllers/api/checks_controller.rb
+++ app/controllers/api/checks_controller.rb
@@ -5,11 +5,11 @@ module Api
     skip_before_action :verify_authenticity_token, only: :create
 
     def create
-      repository = Repository.find_by(full_name: params[:repository][:full_name])
+      repository = Repository.find_by(github_id: params[:repository][:github_id])
 
       return head :not_found if repository.nil?
 
-      check = repository.checks.create
+      check = repository.checks.build
       RepositoryCheckJob.perform_later(check)
       head :ok
     end
