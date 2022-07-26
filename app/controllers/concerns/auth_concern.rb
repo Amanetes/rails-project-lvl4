c1653bc19ed4f138cf12f8f2078ae8149d0ec10d
Sergey <ser_8904@mail.ru>
2022-07-26 20:55:33 +0300

    change AuthConcern

--- app/controllers/concerns/auth_concern.rb
+++ app/controllers/concerns/auth_concern.rb
@@ -3,33 +3,27 @@
 module AuthConcern
   extend ActiveSupport::Concern
 
-  included do
-    private
+  def sign_in(user)
+    session[:user_id] = user.id
+  end
 
-    def sign_in(user)
-      session[:user_id] = user.id
-    end
+  def sign_out
+    session.delete(:user_id)
+    session.clear
+  end
 
-    def sign_out
-      session.delete(:user_id)
-      session.clear
-    end
+  def signed_in?
+    session[:user_id].present? && current_user.present?
+  end
 
-    def signed_in?
-      session[:user_id].present? && current_user.present?
-    end
+  def current_user
+    @current_user ||= User.find_by(id: session[:user_id])
+  end
 
-    def current_user
-      @current_user ||= User.find_by(id: session[:user_id])
-    end
+  def authenticate_user!
+    return if signed_in?
 
-    def authenticate_user!
-      return if signed_in?
-
-      flash[:warn] = 'Необходима аутентификация!'
-      redirect_to root_path
-    end
-
-    helper_method :current_user, :signed_in?
+    flash[:warn] = 'Необходима аутентификация!'
+    redirect_to root_path
   end
 end
