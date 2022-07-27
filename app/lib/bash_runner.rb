bb68110e6266c380f255b648cfe1415212817395
Sergey <ser_8904@mail.ru>
2022-07-27 16:02:59 +0300

    try to fix empty output

--- app/lib/bash_runner.rb
+++ app/lib/bash_runner.rb
@@ -3,10 +3,9 @@
 class BashRunner
   class << self
     def run(cmd)
-      output = Open3.popen3(cmd) do |_stdin, stdout, _stderr, _wait_thr|
+      Open3.popen3(cmd) do |_stdin, stdout, _stderr, _wait_thr|
         stdout.read
       end
-      JSON.parse(output.presence || '{}')
     end
   end
 end
