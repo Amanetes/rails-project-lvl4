951b4c8804d8a1851383753f9431a7a8a183b44d
Sergey <ser_8904@mail.ru>
2022-07-27 11:59:36 +0300

    update stub

--- app/lib/repository_check_manager_stub.rb
+++ app/lib/repository_check_manager_stub.rb
@@ -2,6 +2,16 @@
 
 class RepositoryCheckManagerStub
   class << self
-    def check(repository); end
+    def check(_repository)
+      { issues: [''], issues_count: 0 }
+    end
+
+    def run_eslint_check(_path)
+      { issues: [''], issues_count: 0 }
+    end
+
+    def run_rubocop_check(_path)
+      { issues: [''], issues_count: 0 }
+    end
   end
 end
