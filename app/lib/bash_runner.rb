d01129c351e85cff2db8662f60dec9113682d289
Sergey <ser_8904@mail.ru>
2022-07-27 14:02:47 +0300

    another try

--- app/lib/bash_runner.rb
+++ app/lib/bash_runner.rb
@@ -3,9 +3,10 @@
 class BashRunner
   class << self
     def run(cmd)
-      Open3.popen3(cmd) do |_stdin, stdout, _stderr, _wait_thr|
-        stdout.read
+      stdout, stderr, exit_status = Open3.popen3(cmd) do |_stdin, stdout, stderr, wait_thr|
+        [stdout.read, stderr.read, wait_thr.value]
       end
+      [stdout, stderr, exit_status.exitstatus]
     end
   end
 end
