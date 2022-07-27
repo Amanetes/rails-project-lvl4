f9cb973062974cfd98b4ef7f004e88be5d18bb2f
Sergey <ser_8904@mail.ru>
2022-07-27 12:45:13 +0300

    uncomment home controller test

--- test/controllers/web/home_controller_test.rb
+++ test/controllers/web/home_controller_test.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-# require 'test_helper'
-# module Web
-#   class HomeControllerTest < ActionDispatch::IntegrationTest
-#     test 'should GET home#index' do
-#       get root_path
-#       assert_response :success
-#       assert_select 'title', full_title
-#     end
-#   end
-# end
+require 'test_helper'
+module Web
+  class HomeControllerTest < ActionDispatch::IntegrationTest
+    test 'should GET home#index' do
+      get root_path
+      assert_response :success
+      assert_select 'title', full_title
+    end
+  end
+end
