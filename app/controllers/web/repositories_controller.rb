164de478013019ce759b367e2a4770aca9fe852b
Sergey <ser_8904@mail.ru>
2022-08-01 12:39:46 +0300

    fix issues v1

--- app/controllers/web/repositories_controller.rb
+++ app/controllers/web/repositories_controller.rb
@@ -15,8 +15,9 @@ module Web
       @repository = current_user.repositories.build
       client = ApplicationContainer[:octokit_client_api].new(current_user.token)
       available_languages = Repository.language.values
-      @remote_repositories = Rails.cache.fetch("user_#{current_user.id}_repos", expires_in: 12.hours) do
-        client.repositories.select do |repo|
+      @remote_repositories = Rails.cache.fetch("#{current_user.id}_repositories", expires_in: 12.hours) do
+        client.repositories.select { |repo| repo[:language].present? }
+              .select do |repo|
           available_languages.include?(repo[:language]&.downcase) &&
             current_user.repositories.map(&:name).exclude?(repo[:name])
         end
